36e1851f0f9391e7ba97125e584f55401703df55
pranavi moturu <pm487@afsconnect1.njit.edu>
2017-11-05 09:40:15 -0500

    table display

--- index.php
+++ index.php
@@ -1,25 +1,48 @@
-
 <?php
-
-$servername = "sql2.njit.edu";
-$username = "pm487";
-$database = "pm487";
-$password = "kKmilXOt";
-
-//  Create a new connection to the MySQL database using PDO
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) 
-{
-    die("Connection failed: " . $conn->connect_error);
-    echo "<br>";
-} 
-else
-{
-echo "Connected successfully <br>";
+$dsn      = 'mysql:dbname=pm487;host=sql2.njit.edu';
+$user     = 'pm487';
+$password = 'kKmilXOt';
+try
+ {
+    echo '<table border= "3">';
+    $dbh = new PDO($dsn, $user, $password);
+    echo 'Connected successfully <br>';
+    
+    $link = mysql_connect("sql2.njit.edu", "pm487", "kKmilXOt");
+mysql_select_db("pm487", $link);
+$result = mysql_query("SELECT * FROM accounts where id<6", $link);
+$num_rows = mysql_num_rows($result);
+echo "number of records is $num_rows \n";
+$sql = "SELECT * FROM accounts WHERE id < 6" ;
+    echo "<tr> 
+          <th>ID</th> 
+          <th>EMAIL</th> 
+          <th>FIRSTNAME</th> 
+          <th>LASTNAME</th> 
+          <th>PHONE </th> 
+          <th>BIRTHDAY</th> 
+          <th>GENDER</th> 
+          <th>PASSWORD</th> 
+          </tr>";
+    foreach ($dbh->query($sql) as $row)
+     {
+        echo "<tr>";
+        echo ' <td >'. $row[id] .' </td> 
+               <td>'.$row[email].' </td> 
+               <td>'.$row[fname].' </td> 
+               <td>'.$row[lname].' </td> 
+               <td>'.$row[phone].' </td> 
+               <td>'.$row[birthday].'</td> 
+               <td>'.$row[gender].' </td> 
+               <td>'.$row[password].'</td>';
+        echo "</tr>";
+    }
 }
-?>
-
-
+catch (PDOException $e)
+ {
+    echo "Error: " . $e->getMessage();
+}
+//$dbh = null;
+echo "</table>";
+?>  
 
